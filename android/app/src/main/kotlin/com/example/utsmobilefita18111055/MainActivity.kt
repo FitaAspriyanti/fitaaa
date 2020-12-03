5b1fc32eafc52598ff5e415723375a0ec0e191d8
LAPTOP-RNJGAHM5\ASUS <fitaas30@gmail.com>
2020-12-03 11:55:52 +0700

    baruuuu

---
+++ android/app/src/main/kotlin/com/example/utsmobilefita18111055/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.utsmobilefita18111055
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:utsmobilefita18111055/home.dart';
+
+class SplashScreen extends StatefulWidget {
+  _SplashScreen createState() => _SplashScreen();
+}
+
+class _SplashScreen extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    splashscreenStart();
+  }
+
+  splashscreenStart() async {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Home()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Image.asset(
+                "assets/images/gambar.png",
+                width: size.width * 0.9,
+              ),
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            Text(
+              "Copyright Fita Aspriyanti, 2020",
+              style: TextStyle(
+                color: Colors.brown,
+                fontSize: 20.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
